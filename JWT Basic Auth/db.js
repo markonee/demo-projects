ad9b073e42bd0440d88d143868eaf2cd5fa319de
unknown <mnedic1@etf.unsa.ba>
2019-10-22 14:52:54 +0200

    JWT Auth

---
+++ JWT Basic Auth/db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jwttestuser:WblUT0cUk9MjWsWC@cluster0-cdkbb.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true });
